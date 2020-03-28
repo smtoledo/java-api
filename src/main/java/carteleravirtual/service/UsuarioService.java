98f177bf7a52c9fe22b8c816cd5d5621d3c5b451
stoledo.produccion <laibach2019>
2020-03-28 16:00:19 -0300

    clean

--- src/main/java/carteleravirtual/service/UsuarioService.java
+++ src/main/java/carteleravirtual/service/UsuarioService.java
@@ -9,10 +9,8 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 
-import carteleravirtual.common.Perfil;
 import carteleravirtual.common.TokenValidator;
 import carteleravirtual.dao.UsuarioDAO;
-import carteleravirtual.dto.CarteleraDTO;
 import carteleravirtual.dto.UsuarioDTO;
 import carteleravirtual.model.Usuario;
 
